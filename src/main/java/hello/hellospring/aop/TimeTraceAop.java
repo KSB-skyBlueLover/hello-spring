36816b4478571d94d2c1123a038f1bfde1f6e073
KSB-skyBlueLover <gogotkdqja@yonsei.ac.kr>
2022-04-04 00:03:33 +0900

    use AOP

--- src/main/java/hello/hellospring/aop/TimeTraceAop.java
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 public class TimeTraceAop {
 
     @Around("execution(* hello.hellospring..*(..))")
+//    @Around("execution(* hello.hellospring.service..*(..))")
     public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
         System.out.println("START = " + joinPoint.toString());
